86551ff3fa126d0ffbdfca0dc00878831a8409d2
Tuomas Rinne <torinne@gmail.com>
2023-10-22 12:56:12 +0300

    Fix link paths

--- links.sh
+++ links.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sudo ln -s ~/dotfiles/.lein ~/.lein
-sudo ln -s ~/dotfiles/.shadow-cljs ~/.shadow-cljs
-sudo ln -s ~/dotfiles/.zshrc ~/.zshrc
+sudo ln -s ~/.dotfiles/.lein ~/.lein
+sudo ln -s ~/.dotfiles/.shadow-cljs ~/.shadow-cljs
+sudo ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
