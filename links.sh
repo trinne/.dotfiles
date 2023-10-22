d7ee48375b501c8baecae3d08980f5d00d51c79e
Tuomas Rinne <torinne@gmail.com>
2023-10-22 13:41:51 +0300

    Add nvim config to links

--- links.sh
+++ links.sh
@@ -3,3 +3,4 @@
 sudo ln -s ~/.dotfiles/.lein ~/.lein
 sudo ln -s ~/.dotfiles/.shadow-cljs ~/.shadow-cljs
 sudo ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+sudo ln -s ~/.dotfiles/nvim/.config/nvim ~/.config/nvim
